01a41aadc5840e4fd7dfc0f9f939251b9ff731f5
Felix-Otieno <omondifelixotieno2@gmail.com>
2024-09-24 08:54:56 +0300

    exercise

---
+++ tuesday.py
@@ -0,0 +1,59 @@
+# print(0b101)
+# print(0o10)
+# print(0x1F)
+
+# z = complex(1.25)
+# print(z)
+
+# print(int(2.999))
+
+# import math
+# print(math.ceil(252.4))
+# print(math.floor(252.4))
+
+# print(abs(-45.300))
+
+# from numbers import Number
+# from decimal import Decimal
+# from fractions import Fraction
+
+# print(isinstance(2.0, Number))
+# print(isinstance(Decimal('2.0'), Number))
+# print(isinstance(Fraction(2, 1), Number))
+# print(isinstance("2", Number))
+
+# print(round(100.2563, 3))
+# print(round(100.000056, 3))
+
+# x = -5j
+# print(type(x))
+# strOne = str("pynative")
+# strTwo = "pynative"
+# print(strOne == strTwo)
+# print(strOne is strTwo)
+
+# str1 = "my isname isisis jameis isis bond";
+# sub = "is";
+# print(str1.count(sub, 4))
+
+# str1 = 'Welcome'
+# print(str1*2)
+
+# print("John" > "Jhon")
+# print("Emma" < "Emm")
+
+# str1 = "PYnative"
+# print(str1[1:4], str1[:5], str1[4:], str1[0:-1], str1[:-1])
+# str1 = 'Welcome'
+# print (str1[:6] + ' PYnative')
+
+# str = "My salary is 7000";
+# print(str.isalnum())
+# myString = "pynative"
+# stringList = ["abc", "pynative", "xyz"]
+
+# print(stringList[1] == myString)
+# print(stringList[1] is myString)
+
+str = "my name is James bond";
+print (str.capitalize())
